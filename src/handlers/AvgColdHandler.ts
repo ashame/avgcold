e92c9359e4ece191e1a8a1c636935ce09bb4c612
ashame <ashame@users.noreply.github.com>
2022-04-29 16:22:44 -0700

    Added AvgColdHandler

---
+++ src/handlers/AvgColdHandler.ts
@@ -0,0 +1,37 @@
+import { Message } from 'discord.js';
+import { error } from 'npmlog';
+import Handler from './Handler';
+
+const regex = /.*avg.?cold.*/gi;
+
+class AvgColdHandler extends Handler {
+    handler = async (msg: Message) => {
+        if (msg.partial)
+            try {
+                await msg.fetch();
+            } catch (e) {
+                error('avg-cold-handler', 'failed to fetch message', e);
+            }
+        if (msg.mentions.has(this.bot.client.user ?? '') || regex.exec(msg.content)) {
+            try {
+                await msg.channel.send("<:avgcold:962151999971950622>");
+            } catch (e) {
+                error('avg-cold-handler', 'failed to send message', e);
+            }
+        }
+    };
+
+    register = (): boolean => {
+        if (this.registered) return false;
+        this.bot.client.on('message', this.handler);
+        return (this.registered = true);
+    }
+    deregister = (): boolean => {
+        if (!this.registered) return false;
+        this.bot.client.off('message', this.handler);
+        return (this.registered = false);
+    }
+
+}
+
+export default AvgColdHandler;
