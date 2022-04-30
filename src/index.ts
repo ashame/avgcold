55819ef167554060a7dfe6414f5dd8a6421d4be6
ashame <ashame@users.noreply.github.com>
2022-04-29 17:44:19 -0700

    Feature: bot now attempts to load custom config from config.json

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import logger from 'npmlog';
 import Bot from './Bot';
 
 const token = process.env.TOKEN ?? new Error('No token found');
+let config;
+
+try {
+    config = require('../config.json');
+} catch { }
 
 if (token instanceof Error) throw token;
 
 logger.enableColor();
 if (process.env.LOG_LEVEL) logger.level = process.env.LOG_LEVEL;
 
-const bot = new Bot(token);
+const bot = new Bot(token, config);
 
 bot.login();
 
