67e9842bf4ace3c3a6e0fb120f41bbe5e5dac13e
ashame <ashame@users.noreply.github.com>
2022-04-29 21:23:35 -0700

    Switch logger stream from process.stderr to process.stdout

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const token = process.env.TOKEN ?? new Error('No token found');
 if (token instanceof Error) throw token;
 
 logger.enableColor();
+logger.stream = process.stdout;
+
 if (process.env.LOG_LEVEL) logger.level = process.env.LOG_LEVEL;
 
 const bot = new Bot(token);
@@ -13,5 +15,5 @@ const bot = new Bot(token);
 bot.login();
 
 bot.once('ready', () => {
-    console.log('avgcold tbh...');
+    logger.info('bot', `Logged in as ${bot.client.user?.tag}`);
 })
